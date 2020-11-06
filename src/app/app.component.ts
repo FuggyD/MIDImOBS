dfb74cef988af9f1f14bbeceb99f81f764c6c1f4
Dominik Tuleweit <dominik@tuleweit.com>
2020-11-06 20:26:16 +0100

    successful websocket connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,9 @@ import { Observable, Subject, from } from 'rxjs';
 import 'rxjs/Rx';
 import {inputById, MIDI_INPUT, MIDI_OUTPUT, outputByName} from '@ng-web-apis/midi';
 import { Input } from './interfaces/input';
-import * as socketIo from 'socket.io-client';
-import * as rpcSock from 'rpc-websockets';
-import { RpcClient } from 'jsonrpc-ts';
-import { MessageService } from './message.service';
-}
+// import * as OBSWebSocket from 'obs-websocket-js';
+import OBSWebSocket from 'obs-websocket-js/dist/obs-websocket';
+
 
 @Component({
   selector: 'app-root',
@@ -23,154 +21,88 @@ export class AppComponent {
   MIDI: any;
   socket: any;
   socketID: any;
-  // serverURL: any = 'ws://127.0.0.1:59650';
-  serverURL: any = 'ws://localhost:59650?token=d9a5f079c06736d551a2c9d945bf47590302b';
-  apiToken: string = 'd9a5f079c06736d551a2c9d945bf47590302b';
+  obs: any;
 
-  rpcClient: any;
   input: any;
   
-
-  // newServer = 'https://sockets.streamlabs.com?token=d9a5f079c06736d551a2c9d945bf47590302b';
-  newServer = 'ws://127.0.0.1:59650/api';
-  // newServer = 'http://127.0.0.1:59650?token=d9a5f079c06736d551a2c9d945bf47590302b';
-
   counter: number = 1;
+  scenes: string[];
+  currentScene: string;
+  bindings: any = [
+    {
+      device: {
+        manufacturer: "MIDIPLUS", 
+        name: "Origin25"
+      },
+      key: 48,
+      action: {
+        type: '',
+        command: 'SetCurrentScene',
+        payload: 'Starting'
+      }
+    }
+]
 
   constructor(
-    // private sock: rpcSock.Client(this.serverURL)
-    private messageService: MessageService
+
   ) {    
     this.initMidiStream();
 
+    this.obs = new OBSWebSocket();
+    this.obs.connect({ address: 'localhost:4444', password: '12345' }).then(() => {
+      console.log(`Success! We're connected & authenticated.`);
 
-    // fe80::4b6:253c:a4cb:ef15, 10.0.1.192, fe80::1e6f:290f:8221:e95b
+      return this.obs.send('GetSceneList');
+    })
+    .then(data => {
+      this.scenes = data.scenes;
+      this.currentScene = data.currentScene;
+        console.log(this.scenes);
+        console.log(data)
 
-    //this.socket = socketIo(this.newServer);
-    //this.socket = new rpcSock.Client(this.newServer)
-    //this.socket.connect();
+        
+        // obs.send('SetCurrentScene', {
+        //     'scene-name': scene.name
+        // });
 
-    //this.openSocket();
+    })
 
-  }
 
-  sendMessage() {
-    if (this.input) {
-      this.messageService.sendMessage(this.input);
-      this.input = '';
-    }
-  }
-
-//   getSocketId() {
-//     console.log(this.socket.id)
-//     console.log(this.socket.connected)
-//   }
-
-//   async request(resourceId, methodName, ...args) {
-//     let requestBody = {
-//         jsonrpc: '2.0',
-//         id: this.counter,
-//         method: methodName,
-//         params: { resource: resourceId, args }
-//     };
-//     console.log('send request')
-//     await this.sendMessage(requestBody);
-//     this.counter++;
-// }
-// async sendMessage(message) {
-//   let requestBody = message;
-//   if (typeof message === 'string') {
-//       try {
-//           requestBody = JSON.parse(message);
-//       } catch (e) {
-//           alert('Invalid JSON');
-//           return;
-//       }
-//   }
-//   console.log(message)
-//   this.socket.emit(JSON.stringify(requestBody), (callback) => {
-//     console.log('callback')
-//     console.log(callback)
-//   });
-// }
-
-//   public getScenes() {
-//     this.request('SourcesService','getSources');
-//   }
-
-//   async openSocket() {
-//     console.log('open socket')
-//     // this.socket = socketIo(this.serverURL);
-
-//     await this.request('TcpServerService', 'auth', this.apiToken);
-//     // this.socket.connect();
-//     console.log(this.socket.connected);
-
-//     // this.socket = new rpcSock.Client(this.serverURL);
-//     // // this.socket = new rpcSock.Client(this.newServer);
-//     // this.socket.connect();
-//     // console.log(this.socket)
-//     // this.socket.on('open', () => {
-//     //   console.log('is open')
-//     // })
-   
+  //   obs.onopen = function() {
+  //     console.log('open');
+  //     obs.send('test');
+  // };
   
-//     // this.socket.on('open', function(huhu) {
-//     //   this.socket.login({token: this.apiToken})
-//     //   this.socket.call('sum', [5, 3]).then(function(result) {
-//     //     // require('assert').equal(result, 8)
-//     //   })
-//     // })
+  // obs.onmessage = function(e) {
+  //     console.log('message', e.data);
+  //     obs.close();
+  // };
+  
+  // obs.onclose = function() {
+  //     console.log('close');
+  // };
 
-//     let authReq = {
-//       "jsonrpc": "2.0",
-//       "id": 5,
-//       "method": "auth",
-//       "params": {
-//           "resource": "TcpServerService",
-//           "args": [
-//               "d9a5f079c06736d551a2c9d945bf47590302b"
-//           ]
-//       }
-//     }
+  }
 
-//     // this.socket.emit('', authReq);
 
-//     // this.socket.emit(authReq);
+  pause() {
+    this.obs.send('SetCurrentScene', {
+      'scene-name': 'Pause'
+    });
+  }
 
-//     // this.socket = socketIo(this.serverURL); 
+  starting() {
+    this.obs.send('SetCurrentScene', {
+      'scene-name': 'Starting'
+    });
+  }
 
-//     // this.socket.emit('/onboarding/here-i-am');
-//   }
+  live() {
+    this.obs.send('SetCurrentScene', {
+      'scene-name': 'Live'
+    });
+  }
 
-//   public getMyScenes() {
-//     let sceneReq = {"jsonrpc": "2.0","id": 3,"method": "getScenes","params": {"resource": "ScenesService","args": []}}
-//     console.log('socket call')
-//     this.socket.emit('event',JSON.stringify(sceneReq));
-
-//     // this.socket.emit('', sceneReq);
-//     // this.socket.emit(sceneReq, function (answer) {
-//     //   console.log('answer: ')
-//     //   console.log(answer)
-//     // })
-//     console.log('emitted')
-//   }
-
-//   public sendAuth() {
-//     let auth = {
-//       "jsonrpc": "2.0",
-//       "id": 2,
-//       "method": "auth",
-//       "params": {
-//           "resource": "TcpServerService",
-//           "args": [
-//               "d9a5f079c06736d551a2c9d945bf47590302b"
-//           ]
-//       }
-     
-//   }
-//   this.socket.send(auth);  
-// }
 
   private initMidiStream() {
     const midiAccess$ = from(navigator.requestMIDIAccess());
@@ -199,12 +131,18 @@ export class AppComponent {
           console.log('pushed intensity: ' + input.intense)
           console.log(input)
           console.log(message)
+          if(input.key == 48) {
+            this.pause();
+          } else if (input.key == 50) {
+            this.starting();
+          } else if (input.key == 52) {
+            this.live();
+          }
         }
-        // console.log(message)
-        // this.messages.unshift(message);
-        // this.cd.detectChanges();
       });
   }
+
+
 
   private midiMessageAsObservable(input) {
     const source = new Subject();
--- src/app/services/bindings.service.spec.ts
+++ src/app/services/bindings.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MessageService } from './message.service';
+import { BindingsService } from './bindings.service';
 
-describe('MessageService', () => {
-  let service: MessageService;
+describe('BindingsService', () => {
+  let service: BindingsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(MessageService);
+    service = TestBed.inject(BindingsService);
   });
 
   it('should be created', () => {
